d8d90ec694ea9869dd00b1048502a5db442fefb8
Susana Galan <susanagalanga3@gmail.com>
2018-12-20 22:01:31 -0300

    Commit thymeleaf

---
+++ src/main/java/com/spring/thymeleaf/Application.java
@@ -0,0 +1,16 @@
+package com.spring.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan({"com.spring.thymeleaf.*"})
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
+
